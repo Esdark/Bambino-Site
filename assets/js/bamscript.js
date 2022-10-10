0d55c370e4c5fe4b967e0b0ae438df2ee6332fe9
Arjun Mohanesh <31795399+Esdark@users.noreply.github.com>
2022-10-10 16:57:59 +0530

    menu

---
+++ assets/js/bamscript.js
@@ -0,0 +1,15 @@
+// Mobile Menu
+
+$(document).ready(function ($) {
+
+    $(".close-menu").click(function () {
+      $('.header-menu').collapse('show');
+    });
+    $('.navbar-collapse a').click(function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+    
+  });
+
+
+  
