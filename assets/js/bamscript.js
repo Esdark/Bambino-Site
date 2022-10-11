99e5667b26adab7c0c48e24358976b0953226e61
Arjun Mohanesh <31795399+Esdark@users.noreply.github.com>
2022-10-11 16:17:32 +0530

    animation

--- assets/js/bamscript.js
+++ assets/js/bamscript.js
@@ -1,15 +1,10 @@
 // Mobile Menu
 
 $(document).ready(function ($) {
-
-    $(".close-menu").click(function () {
-      $('.header-menu').collapse('show');
-    });
-    $('.navbar-collapse a').click(function(){
-      $(".navbar-collapse").collapse('hide');
-    });
-    
+  $(".close-menu").click(function () {
+    $(".header-menu").collapse("show");
   });
-
-
-  
+  $(".navbar-collapse a").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+});
