72c8013e467f7002b49ba1d90363cadaad637cd2
Arjun Mohanesh <31795399+Esdark@users.noreply.github.com>
2022-10-18 16:13:50 +0530

    major update

--- assets/js/bamscript.js
+++ assets/js/bamscript.js
@@ -4,7 +4,7 @@ $(document).ready(function ($) {
   $(".close-menu").click(function () {
     $(".header-menu").collapse("show");
   });
-  $(".navbar-collapse a").click(function () {
+  $(".navbar-collapse li a").click(function () {
     $(".navbar-collapse").collapse("hide");
   });
 });
